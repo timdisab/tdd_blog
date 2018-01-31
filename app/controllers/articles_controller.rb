e611c9a7cd11720f82f1fb1fff99c66d8ef12ae4
Tim DiSabatino <tim@pathwaysdigital.com>
2018-01-31 10:47:23 -0800

    Add 404 redirect to index 302 exception

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -22,6 +22,14 @@ class ArticlesController < ApplicationController
     end
   end
 
+  protected
+
+  def resource_not_found
+    message = "The article you are looking for could not be found"
+    flash[:alert] = message
+    redirect_to root_path
+  end
+
   private
 
   def article_params
