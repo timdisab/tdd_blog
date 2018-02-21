963f5fd3eea82b606570ce33452cdb35602fd639
Tim DiSabatino <tim@pathwaysdigital.com>
2018-02-21 15:09:54 -0800

    Add view logic for new articles signin

--- spec/features/listing_article_spec.rb
+++ spec/features/listing_article_spec.rb
@@ -8,6 +8,18 @@ RSpec.feature "Listing Articles" do
     @article2 = Article.create(title: "The second article", body: "Body of second article.", user: john)
   end
 
+  scenario "with articles created and user not signed in" do
+    visit "/"
+
+    expect(page).to have_content(@article1.title)
+    expect(page).to have_content(@article1.body)
+    expect(page).to have_content(@article2.title)
+    expect(page).to have_content(@article2.body)
+    expect(page).to have_link(@article1.title)
+    expect(page).to have_link(@article2.title)
+    expect(page).not_to have_link("New Article")
+  end
+
   scenario "A user lists all articles" do
     visit "/"
 
@@ -18,6 +30,7 @@ RSpec.feature "Listing Articles" do
     expect(page).to have_link(@article1.title)
     expect(page).to have_link(@article2.title)
   end
+
 
 
   scenario "A user has no articles" do
